1d5715449b9863e430dee854d47c7ff3b614ac59
AisingioroHao0 <AisingioroHao0@outlook.com>
2023-06-11 13:55:41 +0800

    更改为服务器监听ip

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using OnlineChatServer;
 var server=new ChatServer();
-// server.Start("10.0.24.12",21101);
-server.Start("127.0.0.1",21101);
+server.Start("10.0.24.12",21101);
+// server.Start("127.0.0.1",21101);
 while(true)
 {
     string str=Console.ReadLine();
